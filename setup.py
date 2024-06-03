0d97074cc14f91300316d0df2369fac39e067983
BSPR0002 <42113148+BSPR0002@users.noreply.github.com>
2024-06-04 07:59:01 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,26 +1,28 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="bsif-webview2",
-    version="1.1.0",
-    author="Blue Sky Infomation Factory",
-    description="Microsoft Webview2 for python.",
-    packages=find_packages(),
-    package_data={
-        "webview": [
+	name="bsif-webview2",
+	version="1.2.0",
+	author="Blue Sky Infomation Factory",
+	description="Microsoft Webview2 for python.",
+	packages=find_packages(),
+	package_data={
+		"webview": [
 			"bridge.js",
 			"*.dll",
 			"runtimes/*/native/WebView2Loader.dll"
 		]
-    },
-    license="BSD 3-Clause License",
-    python_requires=">=3.9",  # Windows 8
-    install_requires=["pythonnet", "pywin32"],
-    classifiers=[
-        "License :: OSI Approved :: BSD License",
-        "Operating System :: Microsoft :: Windows",
-        "Programming Language :: Python :: 3.9",
-    ],
+	},
+	license="BSD 3-Clause License",
+	python_requires=">=3.10",
+	install_requires=["pythonnet", "pywin32"],
+	classifiers=[
+		"License :: OSI Approved :: BSD License",
+		"Operating System :: Microsoft :: Windows :: Windows 10",
+		"Operating System :: Microsoft :: Windows :: Windows 11",
+		"Programming Language :: Python :: 3.10",
+		"Programming Language :: Python :: 3.11"
+	],
 	url="https://github.com/Blue-Sky-Infomation-Factory/Python-Webview2"
 )
 

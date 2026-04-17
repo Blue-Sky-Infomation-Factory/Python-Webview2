399bd59ab50e80f69e0d4f3103614dd6d0d23106
Fina <42113148+BSPR0002@users.noreply.github.com>
2026-04-18 04:14:29 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,15 +1,28 @@
 from threading import Thread
 from time import sleep
+from os.path import dirname
 
-from webview.webview import WebViewApplication
+from bsif_webview.webview import WebViewApplication
 
 app = WebViewApplication(debug_enabled=True)
 
 def delay_test():
-	sleep(10)
-	aa = app.create_window()
-	print(aa)
+	for s in range(5, 0, -1):
+		print(f"Test mission count down: {s}")
+		sleep(1)
+	print("Test mission running")
+	window = app.main_window
+	assert window
+	result = window.show_directory_picker(title="Select a file", initial_directory=dirname(__file__), multiple=True, default_directory_name="test")
+	if result:
+		print(result.directories)
+	app.stop()
 
+# def async_agent(method, args = (), kargs = {}):
+# 	c: Coroutine = method(*args, **kargs)
+# 	result = new_event_loop().run_until_complete(c)
+# 	print(result)
+# Thread(None, async_agent, args=(delay_test,)).start()
 Thread(None, delay_test).start()
 
 app.start(title="test")

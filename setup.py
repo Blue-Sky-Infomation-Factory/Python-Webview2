fa5e611f92295f68d4a9c57abfdf22e82cca9b48
Fina <42113148+BSPR0002@users.noreply.github.com>
2026-04-19 11:14:50 +0800

    Bump to v2 and update setup configuration
    Bump package version to 2.0.0 and overhaul setup.py configuration: rewrite README third-party license links to absolute GitHub paths, replace find_packages() with explicit package list, update package_data key and include libs/webview2/windows_api_code_pack DLL paths, change install_requires from pywin32 to bsif-utils, and simplify Python classifiers to a generic "Python :: 3". Also minor formatting/IO tweaks.

--- setup.py
+++ setup.py
@@ -1,33 +1,50 @@
 from setuptools import setup, find_packages
+import re
 
-with open('README.md', 'r') as file:
+with open("README.md", "r") as file:
     readme = file.read()
+lines = readme.split("\n")
+lines_end = len(lines)
+start_index = None
+for i in range(lines_end):
+    if lines[i] == "## Third-party licenses":
+        start_index = i + 1
+        break
+if start_index is not None:
+    prefix = "https://github.com/Blue-Sky-Infomation-Factory/Python-Webview2/blob/main/"
+    for i in range(start_index, lines_end):
+        line = lines[i]
+        if not line: break
+        href_start = re.search(r"^- \[.*?\]\((.*?)\)$", line).regs[1][0] # type: ignore
+        lines[i] = f"{line[:href_start]}{prefix}{line[href_start:]}"
+readme = "\n".join(lines)
 
 setup(
 	name="bsif-webview2",
-	version="1.5.0",
+	version="2.0.0",
 	author="Blue Sky Infomation Factory",
 	description="Microsoft Webview2 for python.",
 	long_description=readme,
 	long_description_content_type="text/markdown",
-	packages=find_packages(),
+	packages=["bsif_webview", "bsif_webview_tool"],
 	package_data={
-		"webview": [
+		"bsif_webview": [
 			"bridge.js",
-			"*.dll",
-			"runtimes/*/native/WebView2Loader.dll"
+			"libs/*.dll",
+			"libs/webview2/*.dll",
+			"libs/webview2/runtimes/*/native/WebView2Loader.dll",
+			"libs/windows_api_code_pack/*.dll"
 		]
 	},
 	license="BSD 3-Clause License",
 	python_requires=">=3.11",
-	install_requires=["pythonnet", "pywin32"],
+	install_requires=["pythonnet", "bsif-utils"],
 	classifiers=[
 		"License :: OSI Approved :: BSD License",
 		"Operating System :: Microsoft :: Windows :: Windows 10",
 		"Operating System :: Microsoft :: Windows :: Windows 11",
 		"Programming Language :: Python :: Implementation :: CPython",
-		"Programming Language :: Python :: 3.11",
-		"Programming Language :: Python :: 3.12"
+		"Programming Language :: Python :: 3"
 	],
 	url="https://github.com/Blue-Sky-Infomation-Factory/Python-Webview2"
 )
